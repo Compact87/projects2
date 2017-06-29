9e8ad142b2145e954a6d4dd98bbef759b406f066
Uros Veselinovic <UrosVeselinovic@outlook.com>
2017-06-29 14:25:01 +0200

    webapp dir

--- tech01/src/main/java/tech01/Item.java
+++ tech01/src/main/java/tech01/Item.java
@@ -1,18 +1,75 @@
 package tech01;
 
-import java.io.Serializable;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * Entity implementation class for Entity: Item
  *
  */
 @Entity
+@DiscriminatorColumn(name="Discrimin", discriminatorType=DiscriminatorType.CHAR)
+@DiscriminatorValue("I")
+@NamedQuery(name="find all Items", query="SELECT i FROM Item i ORDER BY i.title DESC")
 @Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-public class Item implements Serializable {
+public class Item {
+    
+	/*
+	 * ATTRIBUTES
+	 */
+	
+	
+	@Id@GeneratedValue
+	protected Long id;
+	@NotNull@Size( min=4, max=50)
+	protected String title;
+	@NotNull
+	protected Float price;
+	
+	
+	
+	/* 
+	 * GETTERS/SETTERS
+	 */
+  
+	public String getTitle() {
+		return title;
+	}
+   
 
 	
-	private static final long serialVersionUID = 1L;
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+
+	public Float getPrice() {
+		return price;
+	}
+
+
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+
+	
+	
+	/*
+	 * CONSTRUCTORS
+	 */
+	
+	public Item(String title, Float price) {
+		super();
+		this.title = title;
+		this.price = price;
+	}
+
+
 
 	public Item() {
 		super();
